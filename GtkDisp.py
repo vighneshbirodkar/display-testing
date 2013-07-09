7dd98bf04e640fad176b4671c2e346cbd75f49c4
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2013-07-10 04:29:05 +0530

    added coments, changes example

--- GtkDisp.py
+++ GtkDisp.py
@@ -7,9 +7,13 @@ from time import sleep
 class GtkWorker(Process):
     def __init__(self,connection):
         Process.__init__(self)
+        
+        #The connection used to communicate with the parent
         self.connection = connection
 
     def run(self):
+        #gtk imports need to be local to the process, hence they are in run
+        #otherwise gtk thinks there are multiple copies of itself
         import gtk
         import gobject
         
@@ -19,32 +23,48 @@ class GtkWorker(Process):
         
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.connect("destroy", self.destroy)
+        
+        #a widget to show image
         self.image = gtk.Image()
         
         self.window.add(self.image)
         self.window.show_all()
-
+        
+        #do this when you have time, gtk will call pollMsg whenever it is idle
+        #after doing it's own work
         gobject.idle_add(self.pollMsg,None)
+        
+        #starts gtk
         gtk.main()
         
     def destroy(self,widget,data=None):
         self.gtk.main_quit()
 
     def pollMsg(self,data=None):
+    
+        #check if there is any data to be read, wait for 10ms
+        #Is used because select.select/poll and gobject.idle_add dont work on windows
         dataThere = self.connection.poll(.10)
+        
+        #handle data if it's there
         if(dataThere):
             self.checkMsg()
         return True
     def showImg(self,data):
-
-
+        
+        
+        #show image from string
         pix =  self.gtk.gdk.pixbuf_new_from_data(data['data'], self.gtk.gdk.COLORSPACE_RGB, False, data['depth'], data['width'], data['height'], data['width']*3)
         self.image.set_from_pixbuf(pix)
     
     def checkMsg(self,source=None,condition=None):
-
-        msg = self.connection.recv()
         
+        
+        #examine the message and figure out what to do with it
+        try:
+            msg = self.connection.recv()
+        except IOError:
+            print """Oh No ! there was an IOError """
         
         if(msg['func'] == 'display'):
             self.showImg(msg)
@@ -57,12 +77,14 @@ class GtkWorker(Process):
         
 class GtkDisplay:
     def __init__(self):
+        #each display has its own process
         parentConn,childConn = Pipe()
         self.connection = parentConn
         self.worker = GtkWorker(childConn)
         self.worker.start()
         
     def showImg(self,img):
+        #all functions are implemented with IPC
         dic = {}
         dic['data'] = img.toString()
         dic['depth'] = img.depth
--- example.py
+++ example.py
@@ -6,14 +6,13 @@ from time import sleep
 
 if ( __name__ == '__main__'):
     from SimpleCV import Image,Camera
-    i = Image('lenna')
-    c = 0
+
+    cam = Camera()
+
     disp1 = GtkDisplay()
-    img1 = Image('lenna')
     disp2 = GtkDisplay()
-    img2 = Image('logo')
+
     while(True):
-        c = (c+1)%100
-        d = c*5/100.0 + 1
-        disp1.showImg(img1/d)
-        disp2.showImg(img2/d)
+        img = cam.getImage()
+        disp1.showImg(img)
+        disp2.showImg(img.invert())
