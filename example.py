15891fe4447a9033be9ad80847ed7fcf05ade8ea
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2013-07-11 17:35:21 +0530

    moved socket call

--- example.py
+++ example.py
@@ -5,16 +5,17 @@ print socket.getdefaulttimeout()
 from GtkDisp import *
 
 import socket
-socket.setdefaulttimeout(None)
+
 from time import sleep
 
 
 
 if ( __name__ == '__main__'):
     from SimpleCV import Image,Camera
-
+    socket.setdefaulttimeout(None)
+    
     cam = Camera()
-
+    
     disp1 = GtkDisplay()
     disp2 = GtkDisplay()
 
